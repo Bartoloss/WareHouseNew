bced41b20f4e9d7c866b1b8cbfa68aac81d92dd7
Bartek <rbartek999@gmail.com>
2023-03-13 16:42:07 +0100

    Added Tests

--- TestProject2/ItemManagerTests.cs
+++ TestProject2/ItemManagerTests.cs
@@ -4,15 +4,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using WareHouseNew.App.Abstract;
 using WareHouseNew.App.Concrete;
 using WareHouseNew.App.Managers;
 using WareHouseNew.Domain.Entity;
+using FluentAssertions;
 
 namespace WareHouseNewTests
 {
     public class ItemManagerTests
     {
+
         [Fact]
         public void AddNewItem_AddsProductToTheList()
         {
@@ -20,7 +21,7 @@ namespace WareHouseNewTests
             Item product1 = new Item() { Id = 1, Name = "Product1" };
             var mock = new Mock<ItemService>();
             mock.Setup(s => s.AddItem(product1)).Returns(product1.Id);
-            var manager = new ItemManager(mock.Object, new MenuActionService(), new CategoriesService(), new CategoriesManager(new CategoriesService ()));
+            var manager = new ItemManager(mock.Object, new MenuActionService(), new CategoriesService(), new CategoriesManager(new CategoriesService()));
 
             //Act
             manager.AddNewItem();
@@ -30,5 +31,6 @@ namespace WareHouseNewTests
 
         }
 
+        
     }
 }
