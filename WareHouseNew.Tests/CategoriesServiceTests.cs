4e70d69585118fc411ef0d4b973ba5a8db2cf981
Bartek <rbartek999@gmail.com>
2023-03-14 13:29:22 +0100

    Added Tests

---
+++ WareHouseNew.Tests/CategoriesServiceTests.cs
@@ -0,0 +1,118 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WareHouseNew.App.Concrete;
+using WareHouseNew.Domain.Entity;
+
+namespace WareHouseNew.Tests
+{
+    public class CategoriesServiceTests
+    {
+        [Fact]
+        public void AddItem_AddsCategoryToTheList()
+        {
+            //Arrange
+            CategoriesService categoriesService = new CategoriesService();
+            Categories category = new Categories() { Id = 1, CategoryName = "Category1" };
+
+            //Act
+            int returnedId = categoriesService.AddItem(category);
+
+            //Assert
+            Categories? returnedCategory = categoriesService.GetCategoryById(category.Id);
+            returnedCategory.Should().NotBeNull();
+            returnedCategory.CategoryName.Should().Be(category.CategoryName);
+            returnedCategory.CategoryName.Should().NotBeNull();
+            returnedId.Should().Be(category.Id);
+        }
+
+        [Fact]
+        public void RemoveItem_RemoveCategoryFromTheList()
+        {
+            //Arrange
+            CategoriesService categoriesService = new CategoriesService();
+            Categories category = new Categories() { Id = 1, CategoryName = "Category1" };
+            categoriesService.AddItem(category);
+
+            //Act
+            categoriesService.RemoveItem(category);
+
+            //Assert
+            Categories result = categoriesService.GetCategoryById(category.Id);
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public void GetAllItems_ReturnsListOfAllCategories()
+        {
+            //Arrange
+            CategoriesService categoriesService = new CategoriesService();
+            Categories category1 = new Categories() { Id = 1, CategoryName = "Category1" };
+            Categories category2 = new Categories() { Id = 2, CategoryName = "Category2" };
+            categoriesService.AddItem(category1);
+            categoriesService.AddItem(category2);
+
+            //Act
+            List<Categories> AllCategories = categoriesService.GetAllItems();
+
+            //Assert
+            AllCategories.Should().NotBeNull();
+            AllCategories.Count.Should().Be(2);
+        }
+
+        [Fact]
+        public void GetLastId_ReturnsIdOfLastCategory()
+        {
+            //Arrange
+            CategoriesService categoriesService = new CategoriesService();
+            Categories category1 = new Categories() { Id = 1, CategoryName = "Category1" };
+            Categories category2 = new Categories() { Id = 2, CategoryName = "Category2" };
+            categoriesService.AddItem(category1);
+            categoriesService.AddItem(category2);
+
+            //Act
+            int returnedId = categoriesService.GetLastId();
+
+            //Assert
+            returnedId.Should().Be(category2.Id);
+        }
+
+        [Fact]
+        public void GetCategoryById_ReturnsCategoryOfSelectedId()
+        {
+            //Arrange
+            CategoriesService categoriesService = new CategoriesService();
+            Categories category1 = new Categories() { Id = 1, CategoryName = "Category1" };
+            categoriesService.AddItem(category1);
+
+            //Act
+            Categories returnedProduct = categoriesService.GetCategoryById(category1.Id);
+
+            //Assert
+            returnedProduct.Should().NotBeNull();
+            returnedProduct.Id.Should().Be(category1.Id);
+            returnedProduct.CategoryName.Should().Be(category1.CategoryName);
+
+        }
+
+        [Fact]
+        public void GetNumberOfAllCategories_ReturnsNumberOfAllCategories()
+        {
+            //Arrange
+            CategoriesService categoriesService = new CategoriesService();
+            Categories category1 = new Categories() { Id = 1, CategoryName = "Category1" };
+            Categories category2 = new Categories() { Id = 2, CategoryName = "Category2" };
+            categoriesService.AddItem(category1);
+            categoriesService.AddItem(category2);
+
+            //Act
+            int numberOfCategories = categoriesService.GetNumberOfAllCategories();
+
+            //Assert
+            numberOfCategories.Should().Be(2);
+        }
+    }
+}
---
+++ WareHouseNew.Tests/ItemServiceTests.cs
@@ -0,0 +1,142 @@
+using WareHouseNew.App;
+using WareHouseNew.App.Concrete;
+using WareHouseNew.Domain;
+using WareHouseNew.Domain.Entity;
+using FluentAssertions;
+using System.Xml.Linq;
+
+namespace WareHouseNew.Tests
+{
+    public class ItemServiceTests
+    {
+        [Fact]
+        public void AddItem_AddsProductToTheList()
+        {
+            //Arrange
+            ItemService itemService = new ItemService();
+            Item product = new Item() { Id = 1, Name = "Product1" };
+
+            //Act
+            int returnedId = itemService.AddItem(product);
+
+            //Assert
+            Item returnedProduct = itemService.GetItemById(product.Id);
+            returnedProduct.Should().NotBeNull();
+            returnedProduct.Name.Should().Be(product.Name);
+            returnedProduct.Name.Should().NotBeNull();
+            returnedId.Should().Be(product.Id);
+        }
+
+        [Fact]
+        public void RemoveItem_RemoveProductFromTheList()
+        {
+            //Arrange
+            ItemService itemService = new ItemService();
+            Item product = new Item() { Id = 1, Name = "Product1" };
+            itemService.AddItem(product);
+
+            //Act
+            itemService.RemoveItem(product);
+
+            //Assert
+            Item result = itemService.GetItemById(product.Id);
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public void GetAllItems_ReturnsListOfAllProducts()
+        {
+            //Arrange
+            ItemService itemService = new ItemService();
+            Item product1 = new Item() { Id = 1, Name = "Product1" };
+            Item product2 = new Item() { Id = 2, Name = "Product2" };
+            itemService.AddItem(product1);
+            itemService.AddItem(product2);
+
+            //Act
+            List<Item> AllItems = itemService.GetAllItems();
+
+            //Assert
+            AllItems.Should().NotBeNull();
+            AllItems.Count.Should().Be(2);
+        }
+
+        [Fact]
+        public void GetLastId_ReturnsIdOfLastProduct()
+        {
+            //Arrange
+            ItemService itemService = new ItemService();
+            Item product1 = new Item() { Id = 1, Name = "Product1" };
+            Item product2 = new Item() { Id = 2, Name = "Product2" };
+            itemService.AddItem(product1);
+            itemService.AddItem(product2);
+
+            //Act
+            int returnedId = itemService.GetLastId();
+
+            //Assert
+            returnedId.Should().Be(product2.Id);
+        }
+
+        [Fact]
+        public void GetItemById_ReturnsProductOfSelectedId()
+        {
+            //Arrange
+            ItemService itemService = new ItemService();
+            Item product = new Item() { Id = 1, Name = "Product1" };
+            itemService.AddItem(product);
+
+            //Act
+            Item returnedProduct = itemService.GetItemById(product.Id);
+
+            //Assert
+            returnedProduct.Should().NotBeNull();
+            returnedProduct.Id.Should().Be(product.Id);
+            returnedProduct.Name.Should().Be(product.Name);
+
+        }
+
+        [Fact]
+        public void GetItemsByCategory_ReturnsProductsByCategory()
+        {
+            //Arrange
+            ItemService itemService = new ItemService();
+            Item product1 = new Item() { Id = 1, Name = "Product1", CategoryId = 1 };
+            Item product2 = new Item() { Id = 2, Name = "Product2", CategoryId = 1 };
+            Item product3 = new Item() { Id = 3, Name = "Product3", CategoryId = 2 };
+            itemService.AddItem(product1);
+            itemService.AddItem(product2);
+            itemService.AddItem(product3);
+
+            //Act
+            List<Item> itemsByCategory = itemService.GetItemsByCategory(1);
+
+            //Assert
+            itemsByCategory.Should().NotBeNull();
+            itemsByCategory.Count.Should().Be(2);
+
+        }
+
+        [Fact]
+        public void GetItemsWithLowStack_ReturnsProductsWithLowStack()
+        {
+            //Arrange
+            ItemService itemService = new ItemService();
+            Item product1 = new Item() { Id = 1, Name = "Product1", Amount = 5 };
+            Item product2 = new Item() { Id = 2, Name = "Product2", Amount = 10 };
+            Item product3 = new Item() { Id = 3, Name = "Product3", Amount = 15 };
+            itemService.AddItem(product1);
+            itemService.AddItem(product2);
+            itemService.AddItem(product3);
+
+            //Act
+            List<Item> productsWithLowStack = itemService.GetItemsWithLowStack();
+
+            //Assert
+            productsWithLowStack.Should().NotBeNull();
+            productsWithLowStack.Count.Should().Be(2);
+
+        }
+
+    }
+}
